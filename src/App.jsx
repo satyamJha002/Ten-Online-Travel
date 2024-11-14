aadf3080c655e54a797eac4bae956d797f0af36b
Mohammed Umar khan <mohammedumarkhan803@gmail.com>
2024-11-14 12:05:30 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
+import ScrollToTop from "./Components/NavBar/ScrollToTop";
+import Home from "./Pages/Home/Home";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+        <ScrollToTop />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
