e08c879168b45cab352c9d75b0ebbc9a51d16f75
Anjish <136132607+anjishkr@users.noreply.github.com>
2024-11-20 12:47:48 +0530

    Add files via upload

---
+++ src/Pages/India/Kerala/Munnar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    munnarOtherTrips,
+    munnarsightseeing,
+    travelPackage as munnar,
+} from "../../../assets/Data/munnar";
+import Card from "../../../Components/Card/Card";
+import "./Munnar.css";
+
+const Munnar = () => {
+    return (
+        <>
+            <div className="munnar-intro">
+                <h1>Munnar</h1>
+                <div className="munnarInfo">
+
+                    <p>
+                        Munnar is the most happening hill station in Kerala in South India located 110 km from the nearest Cochin airport. Situated at 6000 feet above sea level, Munnar has enchanting weather, tea plantations, and scenic views. The serenity spells magic for all who visit here. Treehouses in Munnar are popular with tourists. Munnar tourism is about celebrating the holidays in the cool green environment of the Nilgiri hills of Munnar.
+
+                        The tourists can access several sightseeing spots in Munnar, right from National Parks, ancient churches, dams, lakes, temples, scenic viewing spots, tea plantations & tea museums, cardamom fields, echo spots, waterfalls and various other places where you would love to pay a visit. A perfect resort town with the full galore of flora & fauna. Just perfect for nature lovers who would love to stay in the wonderful Treehouses in Munnar. These would give them lots of reasons to cherish the memorable time spent here enjoying vacations.
+                    </p>
+                </div>
+                <div className="friAndFam">
+                    <h2>Friends & Family Tour</h2>
+                </div>
+                <div className="friAndFamCardsContainer">
+                    {(munnar || []).map((pkg) => (
+                        <Card key={pkg.id} pkg={pkg} />
+                    ))}
+                </div>
+                <div className="Sightseeing">
+                    <h2>Sightseeing</h2>
+                </div>
+                <div className="munnnarCardsContainer">
+                    {(munnarsightseeing || []).map((pkg) => (
+                        <Card key={pkg.id} pkg={pkg} />
+                    ))}
+                </div>
+                <div className="Sightseeing">
+                    <h2>Other Trips</h2>
+                </div>
+                <div className="munnerCardsContainer">
+                    {(munnarOtherTrips || []).map((pkg) => (
+                        <Card key={pkg.id} pkg={pkg} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Munnar;
