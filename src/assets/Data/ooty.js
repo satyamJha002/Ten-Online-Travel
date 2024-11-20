ea98e6c25faf84cdbd3abd49f7428622ce0ce495
Anjish <136132607+anjishkr@users.noreply.github.com>
2024-11-20 11:17:57 +0530

    Add files via upload

---
+++ src/assets/Data/ooty.js
@@ -0,0 +1,42 @@
+export const travelPackage = [
+    {
+        id: 1,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2020/11/Ooty.jpg",
+            "https://b4holiday.com/wp-content/uploads/2020/11/Kodaikanal_Pillar_Rocks.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Kerala_Cuisine.jpg",
+
+
+        ],
+        title: "5 Days Kerala Holiday Package",
+        location: "Alleppey, India, Kerala, Munnar, Thekkady",
+        duration: "5 Days - 4 Nights",
+        people: "2 People",
+        discount: "19%",
+        price: "₹12,990",
+        originalPrice: "₹15,990",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+    },
+    {
+        id: 2,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2020/11/Ooty.jpg",
+            "https://b4holiday.com/wp-content/uploads/2020/11/Kodaikanal_Pillar_Rocks.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Kerala_Cuisine.jpg",
+
+
+        ],
+        title: "5 Days Kerala Holiday Package",
+        location: "Alleppey, India, Kerala, Munnar, Thekkady",
+        duration: "5 Days - 4 Nights",
+        people: "2 People",
+        discount: "19%",
+        price: "₹12,990",
+        originalPrice: "₹15,990",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+    },
+
+];
+
