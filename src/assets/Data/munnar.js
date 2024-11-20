d86cb10016aa49f7e9b48a7ca5a9bf8a58dd823b
Anjish <136132607+anjishkr@users.noreply.github.com>
2024-11-20 12:46:45 +0530

    Add files via upload

---
+++ src/assets/Data/munnar.js
@@ -0,0 +1,196 @@
+export const travelPackage = [
+    {
+        id: 1,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2022/02/1565782355_kerala_app-1024x576.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+
+        ],
+        title: "5 Days Kerala Holiday Package",
+        location: "Alleppey, India, Kerala, Munnar, Thekkady",
+        duration: "5 Days - 4 Nights",
+        people: "2 People",
+        discount: "19% Off",
+        price: "₹12,990",
+        originalPrice: "₹15,990",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Tamil Nadu Tamil Nadu- A land of..."
+    },
+    {
+        id: 2,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+
+        ],
+        title: "6 Days Kerala Holiday Package",
+        location: "Alleppey, India, Kerala, Kochi, Munnar, Thekkady",
+        duration: "6 Days - 5 Nights",
+        people: "2 People",
+        discount: "21% Off",
+        price: "₹14,900",
+        originalPrice: "₹18,900",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Tamil Nadu Tamil Nadu- A land of..."
+    },
+    {
+        id: 3,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+        ],
+        title: "4 Days Kerala Holiday Package",
+        location: "Alleppey, Kerala, Munnar",
+        duration: "4 Days - 3 Nights",
+        people: "2 People",
+        discount: "12% Off",
+        price: "₹11,900",
+        originalPrice: "₹13,500",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Tamil Nadu Tamil Nadu- A land of..."
+    },
+];
+
+export const munnarsightseeing = [
+    {
+        id: 1,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+        ],
+        title: "6 Days Kerala Holiday Package",
+        location: "Alleppey, India, Kerala, Kochi, Munnar, Thekkady",
+        duration: "6 Days - 5 Nights",
+        people: "2 People",
+        discount: "21% Off",
+        price: "₹14,900",
+        originalPrice: "₹18,900",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Kerala – a divine state in the..."
+    },
+    {
+        id: 2,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+        ],
+        title: "4 Days Kerala Holiday Package",
+        location: "Alleppey, Kerala, Munnar",
+        duration: "4 Days - 3 Nights",
+        people: "2 People",
+        discount: "12% Off",
+        price: "₹11,900",
+        originalPrice: "₹13,500",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Kerala – a divine state in the..."
+
+    },
+    {
+        id: 3,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/09/Abad_hotels.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/09/Abad_Harmonia_Kovalam_1.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/09/Abad_Harmonia_Kovalam_4.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/09/Abad_copper_castle-1024x576.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/09/Abad_copper_castle_2-1024x576.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/09/Bamboo_pool_villa_in_Abad_Whispering_Palms_Resort_in_Kumarakom.jpg",
+        ],
+        title: "Abad Hotels Kerala Packages 7 Night",
+        location: "Alleppey, Kerala, Kochi, Kovalam, Kumarakom, Munnar, Thekkady",
+        duration: "8 Days - 7 Nights",
+        people: "2-6 People",
+        discount: "10% Off",
+        price: "₹27,990",
+        originalPrice: "₹31,150",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Abad Hotels Kerala Packages..."
+    },
+    {
+        id: 4,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+        ],
+        title: "Kerala Honeymoon Package – Deluxe",
+        location: "India, Kerala, Munnar",
+        duration: "8 Days - 7 Nights",
+        people: "2-8 People",
+        discount: "16% Off",
+        price: "₹24,500",
+        originalPrice: "₹29,000",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Kerala has an endless list of tourist..."
+    },
+    {
+        id: 5,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Munnar_Tea_Gardens-2.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/16039574381881.jpeg",
+        ],
+        title: "Kerala Full Tour Packages",
+        location: "Alleppey, India, Kanyakumari, Kerala, Kochi, Kovalam, Munnar, Thekkady",
+        duration: "9 Days - 8 Nights",
+        people: "2-8 People",
+        discount: "34% Off",
+        price: "₹27,900",
+        originalPrice: "₹42,000",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "Kerala Full Tour Packages is embraced..."
+    },
+
+
+];
+
+export const munnarOtherTrips = [
+
+
+    {
+        id: 1,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2022/07/dream-catcher-treehouse-plantation-resort.jpg",
+            "https://b4holiday.com/wp-content/uploads/2022/02/1565782355_kerala_app-1024x576.jpg",
+            "https://b4holiday.com/wp-content/uploads/2022/07/Bamboo-dale-tree-house.jpg",
+            "https://b4holiday.com/wp-content/uploads/2022/07/naturezone-jungle-resort-munnar.jpg",
+            "https://b4holiday.com/wp-content/uploads/2021/02/Houseboat_in_Alleppey_n_Kumarakom_Backwaters.jpg",
+
+        ],
+        title: "Kerala Tree House Honeymoon Package 3 Night",
+        location: "Alleppey, Kerala, Munnar",
+        duration: "4 Days - 3 Nights",
+        people: "2-6 People",
+        discount: "20% Off",
+        price: "₹17,500",
+        originalPrice: "₹22,000",
+        nextDepartures: ["Nov 19", "Nov 20", "Nov 21"],
+        featured: true,
+        description: "A rare and unique honeymoo..."
+    },
+];
