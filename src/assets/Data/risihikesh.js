11fa294bb31bdc48c7514ab5c12dcd1b4d02581b
Bivekanand007 <96653787+Bivekanand007@users.noreply.github.com>
2024-11-21 09:16:56 +0530

    Create risihikesh.js
    updated

---
+++ src/assets/Data/risihikesh.js
@@ -0,0 +1,196 @@
+export const rishikeshcamping = [
+    {
+      id: 1,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/river_rafting_rishikesh.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-12.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-10.jpg",
+      ],
+      title: "Rishikesh Camping",
+      location: "India, Rishikesh, Uttarakhand ",
+      duration: "2 Days - 1 Nights",
+      discount: "33% Off ",
+      people: "2-60 People",
+      price: "₹1,500",
+      originalPrice: "₹2,250",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+    },
+  ];
+  export const rishikeshfriend = [
+    {
+        id: 1,
+        images: [
+            "https://b4holiday.com/wp-content/uploads/2021/12/Vilamendhoo-Island-2-600x400-1.jpg",
+          "https://b4holiday.com/wp-content/uploads/2021/12/Vilamendhoo-Island-3-600x400-1.jpg",
+          "https://b4holiday.com/wp-content/uploads/2021/12/SwimmingPool-2-800x450-1.jpg",
+          "https://b4holiday.com/wp-content/uploads/2021/12/Jacuzzi-Water-Villa-5-600x400-1.jpg",
+          "https://b4holiday.com/wp-content/uploads/2021/12/Jacuzzi-Water-Villa-3-600x400-1.jpg",
+          "https://b4holiday.com/wp-content/uploads/2021/12/Vilamendhoo-Aerial-4-600x400-1.jpg",
+        ],
+        title: "Uttarakhand (Haridwar-Mussoorie-Nainital)",
+        location: "Mussoorie,Nainital,Rishikesh,Uttarakhand",
+        duration: "6 Days - 5 Nights",
+        people: "4 People",
+        discount: "12% Off",
+        price: "₹60,000",
+        originalPrice: "₹68,000",
+        nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+        featured: true,
+        description: "Uttarakhand tourism offerings range from the best of natural splendours to unrivalled adrenaline-inducing adventures. But..."
+    },
+    
+  ];
+  
+  export const rishikeshhiking = [
+    {
+      id: 1,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_20180420_171651732_HDR.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
+      ],
+      title: "Rishikesh Rafting & Camping | 2 Nights Stay",
+      location: "Rishikesh",
+      duration: "3 Days - 2 Nights",
+      discount: "16 Off%",
+      people: "2-60 People",
+      price: "₹2,950",
+     originalPrice: "₹3,500",
+     nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Camping is one of those recreational activities which is best to....."
+
+    },
+    {
+      id: 2,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_20180420_171651732_HDR.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
+      ],
+      title: "Rishikesh Camping | Wild Leopard Camp",
+     location: "Rishikesh",
+      duration: "2 Days - 1 Nights",
+      discount: "36% Off",
+      people: "2-60 People",
+      price: "₹1,450",
+    originalPrice: "2,250",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+
+
+    },
+    {
+    id: 3,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_20180420_171651732_HDR.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
+      ],
+      title: "Rishikesh Camping Package",
+      location: "Rishikesh",
+      duration: "3 Days - 2 Nights",
+      discount: "23% Off",
+      people: "2-60 People",
+      price: "₹2,900",
+    originalPrice: "₹3,750",
+      nextDepartures: ["Nov 20, Nov 21, Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+
+
+    },
+  ];
+  export const rishikeshrafting = [
+    {
+      id: 1,
+      images: [
+       "https://b4holiday.com/wp-content/uploads/2021/03/river_rafting_rishikesh.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-12.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-10.jpg",
+      ],
+      title: "Rishikesh Camping",
+      location: "India, Rishikesh, Uttarakhand ",
+      duration: "2 Days - 1 Nights",
+      discount: "33% Off ",
+      people: "2-60 People",
+      price: "₹1,500",
+      originalPrice: "₹2,250",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+
+    },
+    {
+      
+      id: 2,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_20180420_171651732_HDR.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
+      ],
+      title: "Rishikesh Rafting & Camping | 2 Nights Stay",
+      location: "Rishikesh",
+      duration: "3 Days - 2 Nights",
+      discount: "16 Off%",
+      people: "2-60 People",
+      price: "₹2,950",
+     originalPrice: "₹3,500",
+     nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Camping is one of those recreational activities which is best to....."
+
+    },
+  ];
+    export const  rishikeshwatersports= [
+      {
+        id: 1,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_20180420_171651732_HDR.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
+      ],
+      title: "Rishikesh Rafting & Camping | 2 Nights Stay",
+      location: "Rishikesh",
+      duration: "3 Days - 2 Nights",
+      discount: "16 Off%",
+      people: "2-60 People",
+      price: "₹2,950",
+     originalPrice: "₹3,500",
+     nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Don't miss the best Camping experience in Rishikesh. Camping is one of those recreational activities which is best to....."
+      }
+
+    ];
