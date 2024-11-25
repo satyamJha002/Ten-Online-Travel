e045fc7cc310afa5a5ea877eda663ecd5f945459
Bivekanand007 <96653787+Bivekanand007@users.noreply.github.com>
2024-11-25 16:05:42 +0530

    Update risihikesh.js
    updated

--- src/assets/Data/risihikesh.js
+++ src/assets/Data/risihikesh.js
@@ -11,7 +11,7 @@ export const rishikeshcamping = [
         "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-10.jpg",
       ],
       title: "Rishikesh Camping",
-      location: "India, Rishikesh, Uttarakhand ",
+      location: "Rishikesh, Uttarakhand ",
       duration: "2 Days - 1 Nights",
       discount: "33% Off ",
       people: "2-60 People",
@@ -19,37 +19,35 @@ export const rishikeshcamping = [
       originalPrice: "₹2,250",
       nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+      description: "Experience the ultimate camping adventure in Rishikesh..."
     },
-  ];
-  export const rishikeshfriend = [
+
     {
-        id: 1,
-        images: [
-            "https://b4holiday.com/wp-content/uploads/2021/12/Vilamendhoo-Island-2-600x400-1.jpg",
-          "https://b4holiday.com/wp-content/uploads/2021/12/Vilamendhoo-Island-3-600x400-1.jpg",
-          "https://b4holiday.com/wp-content/uploads/2021/12/SwimmingPool-2-800x450-1.jpg",
-          "https://b4holiday.com/wp-content/uploads/2021/12/Jacuzzi-Water-Villa-5-600x400-1.jpg",
-          "https://b4holiday.com/wp-content/uploads/2021/12/Jacuzzi-Water-Villa-3-600x400-1.jpg",
-          "https://b4holiday.com/wp-content/uploads/2021/12/Vilamendhoo-Aerial-4-600x400-1.jpg",
-        ],
-        title: "Uttarakhand (Haridwar-Nainital)",
-        location: "Nainital,Uttarakhand",
-        duration: "6 Days - 5 Nights",
-        people: "4 People",
-        discount: "12% Off",
-        price: "₹60,000",
-        originalPrice: "₹68,000",
-        nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
-        featured: true,
-        description: "Uttarakhand tourism offerings range from the best of natural splendours to unrivalled adrenaline-inducing adventures. But..."
+      
+      id: 2,
+      images: [
+        "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/11.jpeg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-12.jpg",
+        "https://b4holiday.com/wp-content/uploads/2021/03/2018-03-10.jpg",
+      ],
+      title: "Uttarakhand Nainital",
+      location: "Nainital, Uttarakhand ",
+      duration: "6 Days - 5 Nights",
+      discount: "33% Off ",
+      people: "4 People",
+      price: "₹1,500",
+      originalPrice: "₹2,250",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      featured: true,
+      description: "Uttarakhand tourism offers everything from breathtaking natural beauty..."
     },
     
-  ];
-  
-  export const rishikeshhiking = [
+
     {
-      id: 1,
+      id: 3,
       images: [
         "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
@@ -59,20 +57,20 @@ export const rishikeshcamping = [
         "https://b4holiday.com/wp-content/uploads/2021/03/bonfire.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
       ],
-      title: "Rishikesh Rafting & Camping | 2 Nights Stay",
+      title: "Rishikesh Rafting & Camping",
       location: "Rishikesh",
       duration: "3 Days - 2 Nights",
       discount: "16 Off%",
       people: "2-60 People",
       price: "₹2,950",
-     originalPrice: "₹3,500",
-     nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      originalPrice: "₹3,500",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Camping is one of those recreational activities which is best to....."
+      description: "Experience the ultimate camping adventure in Rishikesh....."
 
     },
     {
-      id: 2,
+      id: 4,
       images: [
         "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
@@ -83,20 +81,20 @@ export const rishikeshcamping = [
         "https://b4holiday.com/wp-content/uploads/2021/03/8.jpeg",
       ],
       title: "Rishikesh Camping | Wild Leopard Camp",
-     location: "Rishikesh",
+      location: "Rishikesh",
       duration: "2 Days - 1 Nights",
       discount: "36% Off",
       people: "2-60 People",
       price: "₹1,450",
-    originalPrice: "2,250",
+      originalPrice: "2,250",
       nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+      description: "Experience the ultimate camping adventure in Rishikesh..."
 
 
     },
     {
-    id: 3,
+    id: 5,
       images: [
         "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
@@ -112,17 +110,16 @@ export const rishikeshcamping = [
       discount: "23% Off",
       people: "2-60 People",
       price: "₹2,900",
-    originalPrice: "₹3,750",
+      originalPrice: "₹3,750",
       nextDepartures: ["Nov 20, Nov 21, Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+      description: "Experience the ultimate camping adventure in Rishikesh..."
 
 
     },
-  ];
-  export const rishikeshrafting = [
+
     {
-      id: 1,
+      id: 6,
       images: [
        "https://b4holiday.com/wp-content/uploads/2021/03/river_rafting_rishikesh.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
@@ -141,12 +138,12 @@ export const rishikeshcamping = [
       originalPrice: "₹2,250",
       nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Rishikesh Camping is one of those recreational activities which is best to..."
+      description: "Experience the ultimate camping adventure in Rishikesh..."
 
     },
     {
       
-      id: 2,
+      id: 7,
       images: [
         "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
@@ -162,16 +159,15 @@ export const rishikeshcamping = [
       discount: "16 Off%",
       people: "2-60 People",
       price: "₹2,950",
-     originalPrice: "₹3,500",
-     nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      originalPrice: "₹3,500",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Camping is one of those recreational activities which is best to....."
+      description: "Experience the ultimate camping adventure in Rishikesh....."
 
     },
-  ];
-    export const  rishikeshwatersports= [
-      {
-        id: 1,
+
+    {
+      id: 8,
       images: [
         "https://b4holiday.com/wp-content/uploads/2021/03/IMG_2256.jpg",
         "https://b4holiday.com/wp-content/uploads/2021/03/20170909_102201.jpg",
@@ -187,10 +183,10 @@ export const rishikeshcamping = [
       discount: "16 Off%",
       people: "2-60 People",
       price: "₹2,950",
-     originalPrice: "₹3,500",
-     nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
+      originalPrice: "₹3,500",
+      nextDepartures: ["Nov 20", "Nov 21", "Nov 22"],
       featured: true,
-      description: "Don't miss the best Camping experience in Rishikesh. Camping is one of those recreational activities which is best to....."
-      }
+      description: "Experience the ultimate camping adventure in Rishikesh...."
+    }
 
     ];
